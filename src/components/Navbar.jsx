8dbaa0c3fd25862d9e852943c1e728d7342a2fcc
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-04-27 21:08:50 +0600

    learn active nav item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,32 @@
-
+"use client"
 
 import Link from 'next/link'
 import React from 'react'
 import { ThemeToggle } from './ThemeToggle'
+import { usePathname } from 'next/navigation'
+import { Inter } from "next/font/google";
 
 function Navbar() {
-    return (
-        <div>
 
-            {/* Basic */}
+    const pathName = usePathname();
+
+    const inter = Inter({ subsets: ["latin"] });
+
+    const activeLink = (path) => {
+        return pathName === path ? " text-blue-500" : ""
+    }
+
+    return (
+        <div className={inter.className}>
+
             <nav className="sticky top-0 z-40 w-full border-b border-separator bg-background/70 backdrop-blur-lg">
                 <header className="flex h-16 items-center justify-between px-6">
                     <div className="flex items-center gap-3">
                         <p className="font-bold">ACME</p>
                     </div>
                     <ul className="flex items-center gap-4">
-                        <li><Link href="/">Home</Link></li>
-                        <li><Link href="/tasks">Tasks</Link></li>
+                        <li><Link className={activeLink("/")} href="/">Home</Link></li>
+                        <li><Link className={activeLink("/tasks")} href="/tasks">Tasks</Link></li>
                     </ul>
                     <div>
                         <ThemeToggle />
